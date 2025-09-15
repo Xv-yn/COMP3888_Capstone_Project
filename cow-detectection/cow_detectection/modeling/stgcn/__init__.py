5ec60afba478ef8150f414efc33cdc8036351fae
htra0336 <htra0336@uni.sydney.edu.au>
2025-09-15 17:14:49 +1000

    chore: clean ignored files from repo

---
+++ cow-detectection/cow_detectection/modeling/stgcn/__init__.py
@@ -0,0 +1,8 @@
+"""
+ST-GCN package initialization.
+
+This package provides the implementation of the Spatial-Temporal Graph
+Convolutional Network (ST-GCN) for cow behavior recognition. It exposes
+the model definition, trainer, inference pipeline, preprocessing utilities,
+and validator for cross-validation.
+"""
